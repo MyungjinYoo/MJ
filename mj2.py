01ae77e5a174f540ca8841157269bbb684b0d2a6
MyungjinYoo <123149344+MyungjinYoo@users.noreply.github.com>
2023-01-20 16:05:02 +0900

    Add files via upload

--- mj2.py
+++ mj2.py
@@ -1,14 +1,8 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 
-
-from matplotlib import font_manager, rc
-font = font_manager.FontProperties(fname="c:/Windows/Fonts/malgun.ttf").get_name()
-rc('font', family=font)
-
-money = pd.read_csv("C:\JAY\data\csvdata\\money_data7.csv")
+money = pd.read_csv("money_data7.csv")
 
 
 import streamlit as st
@@ -28,22 +22,22 @@ fig, ax = plt.subplots(2,2, figsize=(12,8))
 plt.subplot(221)
 plt.plot(  list( money['A_MONTH'] ), list( money['A_RATE'] ), color='red' , marker='o'     ) 
 plt.xticks(tuple(money['A_MONTH']) )
-plt.title('미국금리')
+plt.title('US Interest')
 
 
 plt.subplot(222)
 plt.plot(  list( money['A_MONTH'] ), list( money['K_RATE'] ), color='blue' , marker='o'     ) 
 plt.xticks(tuple(money['A_MONTH']) )
-plt.title('한국금리')
+plt.title('KR Interest')
 
 plt.subplot(223)
 plt.plot(  list( money['A_MONTH'] ), list( money['KOSPI'] ), color='green' , marker='o'     ) 
 plt.xticks(tuple(money['A_MONTH']) )
-plt.title('코스피 지수')
+plt.title('KOSPI')
 
 plt.subplot(224)
 plt.plot(  list( money['A_MONTH'] ), list( money['HOUSE_PRICE'] ), color='yellow' , marker='o'     ) 
 plt.xticks(tuple(money['A_MONTH']) )
-plt.title('집값')
+plt.title('Housing Price')
 
 st.pyplot(fig)
