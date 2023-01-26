e57ef4b1337b5eac717c1f9c7919c287bf25b3f0
MyungjinYoo <123149344+MyungjinYoo@users.noreply.github.com>
2023-01-26 16:57:54 +0900

    Update mj2.py

--- mj2.py
+++ mj2.py
@@ -1,16 +1,17 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-st.title(':heartbeat: :blue[Economic data 2020~2022] :heartbeat:')
-
-money = pd.read_csv("money_data7.csv")
-
-st.sidebar.success("Select a demo above.")
 import streamlit as st
 
-option = st.selectbox(
-    'How would you like to choice year ?',
-    ('2020', '2021', '2022'))
+st.title(':heartbeat: :blue[Economic data 2020~2022] :heartbeat:')
+
+#st.sidebar.success("Select a demo above.")
+
+def  plotting_demo():
+    money = pd.read_csv("money_data7.csv")
+    option = st.selectbox(
+        'How would you like to choice year ?',
+        ('2020', '2021', '2022'))
 
 option2 = int(option)
 
@@ -21,25 +22,30 @@ money = money[:] [money['A_YEAR']== option2]
 fig, ax = plt.subplots(2,2, figsize=(15,10))
 
 plt.subplot(221)
-plt.plot(  list( money['A_MONTH'] ), list( money['A_RATE'] ), color='red' , marker='o'     ) 
+plt.plot( list( money['A_MONTH'] ), list( money['A_RATE'] ), color='red' , marker='o' ) 
 plt.xticks(tuple(money['A_MONTH']) )
 plt.title('US Interest')
 
-
 plt.subplot(222)
-plt.plot(  list( money['A_MONTH'] ), list( money['K_RATE'] ), color='blue' , marker='o'     ) 
+plt.plot( list( money['A_MONTH'] ), list( money['K_RATE'] ), color='blue' , marker='o' ) 
 plt.xticks(tuple(money['A_MONTH']) )
 plt.title('KR Interest')
 
 plt.subplot(223)
-plt.plot(  list( money['A_MONTH'] ), list( money['KOSPI'] ), color='green' , marker='o'     ) 
+plt.plot( list( money['A_MONTH'] ), list( money['KOSPI'] ), color='green' , marker='o' )
 plt.xticks(tuple(money['A_MONTH']) )
 plt.title('KOSPI')
 
 plt.subplot(224)
-plt.plot(  list( money['A_MONTH'] ), list( money['HOUSE_PRICE'] ), color='yellow' , marker='o'     ) 
+plt.plot( list( money['A_MONTH'] ), list( money['HOUSE_PRICE'] ), color='yellow' , marker='o' )
 plt.xticks(tuple(money['A_MONTH']) )
 plt.title('Housing Price')
 
 st.pyplot(fig)
 st.dataframe(money)
+
+with st.form(key ='Form1'):
+    with st.sidebar:
+        select_language = st.sidebar.radio('What do you want ?', ('line', 'bar', 'pie'))
+if select_language =='line':
+    plotting_demo()
