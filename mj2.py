e3490d5400a49d5d2d1ba3d59cb7a9d26df7359a
MyungjinYoo <123149344+MyungjinYoo@users.noreply.github.com>
2023-01-26 17:19:55 +0900

    Update mj2.py

--- mj2.py
+++ mj2.py
@@ -5,7 +5,7 @@ import numpy as np
 
 st.title(':heartbeat: :blue[Economic data 2020~2022] :heartbeat:')
 
-def plotting_demo() :
+def plotting_basic() :
     money = pd.read_csv("money_data7.csv")
     option = st.selectbox( 'How would you like to choice year ?', ('2020', '2021', '2022') )
     option2 = int(option)
@@ -13,24 +13,25 @@ def plotting_demo() :
     money = money[:] [money['A_YEAR']== option2]
     fig, ax = plt.subplots(2,2, figsize=(15,10))
 
+def plotting_line() :
     plt.subplot(221)
-    plt.plot( list( money['A_MONTH'] ), list( money['A_RATE'] ), color='red' , marker='o' ) 
-    plt.xticks(tuple(money['A_MONTH']) )
+    plt.plot( money.A_MONTH , money.A_RATE , color='red' , marker='o' ) 
+    plt.xticks( money.A_MONTH )
     plt.title('US Interest')
 
     plt.subplot(222)
-    plt.plot( list( money['A_MONTH'] ), list( money['K_RATE'] ), color='blue' , marker='o' ) 
-    plt.xticks(tuple(money['A_MONTH']) )
+    plt.plot( money.A_MONTH , money.K_RATE' , color='blue' , marker='o' ) 
+    plt.xticks( money.A_MONTH )
     plt.title('KR Interest')
 
     plt.subplot(223)
-    plt.plot( list( money['A_MONTH'] ), list( money['KOSPI'] ), color='green' , marker='o' )
-    plt.xticks(tuple(money['A_MONTH']) )
+    plt.plot( money.A_MONTH, money.KOSPI, color='green' , marker='o' )
+    plt.xticks( money.A_MONTH )
     plt.title('KOSPI')
 
     plt.subplot(224)
-    plt.plot( list( money['A_MONTH'] ), list( money['HOUSE_PRICE'] ), color='yellow' , marker='o' )
-    plt.xticks(tuple(money['A_MONTH']) )
+    plt.plot( money.A_MONTH, money.HOUSE_PRICE, color='yellow' , marker='o' )
+    plt.xticks( money.A_MONTH )
     plt.title('Housing Price')
 
     st.pyplot(fig)
@@ -39,5 +40,6 @@ def plotting_demo() :
 with st.form(key ='Form1'):
     with st.sidebar:
         select_language = st.sidebar.radio('What do you want ?', ('line', 'bar', 'pie' , 'histogram' , 'corr' , 'wordcloud' , 'box' ))
+plotting_basic()
 if select_language =='line':
-    plotting_demo()
+    plotting_line()
