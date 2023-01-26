99665215cc6af726a077a40e58ef4a35710920d2
MyungjinYoo <123149344+MyungjinYoo@users.noreply.github.com>
2023-01-26 17:28:26 +0900

    Update mj2.py

--- mj2.py
+++ mj2.py
@@ -5,15 +5,14 @@ import numpy as np
 
 st.title(':heartbeat: :blue[Economic data 2020~2022] :heartbeat:')
 
-def plotting_basic() :
+def plotting_line() :
     money = pd.read_csv("money_data7.csv")
     option = st.selectbox( 'How would you like to choice year ?', ('2020', '2021', '2022') )
     option2 = int(option)
     st.write('You selected:', option)
     money = money[:] [money['A_YEAR']== option2]
     fig, ax = plt.subplots(2,2, figsize=(15,10))
-
-def plotting_line() :
+    
     plt.subplot(221)
     plt.plot(  list( money['A_MONTH'] ), list( money['A_RATE'] ), color='red' , marker='o'     ) 
     plt.xticks(tuple(money['A_MONTH']) )
@@ -40,6 +39,5 @@ def plotting_line() :
 with st.form(key ='Form1'):
     with st.sidebar:
         select_language = st.sidebar.radio('What do you want ?', ('line', 'bar', 'pie' , 'histogram' , 'corr' , 'wordcloud' , 'box' ))
-plotting_basic()
 if select_language =='line':
     plotting_line()
