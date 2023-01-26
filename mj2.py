4d72392be3303f90b31da218e522188b5d31a299
MyungjinYoo <123149344+MyungjinYoo@users.noreply.github.com>
2023-01-26 17:29:42 +0900

    Update mj2.py

--- mj2.py
+++ mj2.py
@@ -14,7 +14,7 @@ def plotting_line() :
     fig, ax = plt.subplots(2,2, figsize=(15,10))
     
     plt.subplot(221)
-    plt.plot(  list( money['A_MONTH'] ), list( money['A_RATE'] ), color='red' , marker='o'     ) 
+    plt.plot(  money.A_MONTH , list( money['A_RATE'] ), color='red' , marker='o'     ) 
     plt.xticks(tuple(money['A_MONTH']) )
     plt.title('America rate')
 
