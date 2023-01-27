d4fd09ce6447e9c56ccf478d95689e6f2e1d97f6
MyungjinYoo <123149344+MyungjinYoo@users.noreply.github.com>
2023-01-27 16:58:26 +0900

    Update mj2.py

--- mj2.py
+++ mj2.py
@@ -4,10 +4,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 st.title(':heartbeat: :blue[Economic data 2020~2022] :heartbeat:')
+uploaded_file = st.file_uploader("Choose a file")
+money=pd.read_csv(uploaded_file)
 
 def plotting_line() :
-    uploaded_file = st.file_uploader("Choose a file")
-    money=pd.read_csv(uploaded_file)
+
     #money = pd.read_csv("money_data7.csv")
     option = st.selectbox( 'How would you like to choice year ?', ('2020', '2021', '2022') )
     option2 = int(option)
@@ -43,6 +44,6 @@ with st.form(key ='Form1'):
         select_language = st.sidebar.radio('What do you want ?', ('line', 'bar', 'pie' , 'histogram' , 'corr' , 'wordcloud' , 'box' ))
 if select_language =='line':
     try:
-          plotting_demo()  
-    except:      
+          plotting_demo()
+    except:
           pass
