33d58f1b6f832fa5aa4e92a9c8917e4a246c78a8
MyungjinYoo <123149344+MyungjinYoo@users.noreply.github.com>
2023-01-27 17:03:22 +0900

    Update mj2.py

--- mj2.py
+++ mj2.py
@@ -4,11 +4,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 st.title(':heartbeat: :blue[Economic data 2020~2022] :heartbeat:')
-uploaded_file = st.file_uploader("Choose a file")
-money=pd.read_csv(uploaded_file)
+
 
 def plotting_line() :
-
+    uploaded_file = st.file_uploader("Choose a file")
+    money=pd.read_csv(uploaded_file)
     #money = pd.read_csv("money_data7.csv")
     option = st.selectbox( 'How would you like to choice year ?', ('2020', '2021', '2022') )
     option2 = int(option)
