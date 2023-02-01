e9b5c8957c276385e5827d4b051b6a7579bd05d7
MyungjinYoo <123149344+MyungjinYoo@users.noreply.github.com>
2023-02-01 15:15:11 +0900

    Update mj2.py

--- mj2.py
+++ mj2.py
@@ -54,7 +54,7 @@ def plotting_bar() :
     st.write('You selected:', option)
     baseball['정규순위'] = baseball.groupby('년도')['승률'].rank(ascending = False).astype(int)
     df7  =  baseball[:] [ baseball.년도==option2 ].sort_values(by = '승률' , ascending = False).reset_index()
-    df7 = [['순위','팀','경기수','승','패','무','승률','게임차','연속','출루율','장타율','최근 10경기','년도','정규순위']]
+    df7 = df7[['순위','팀','경기수','승','패','무','승률','게임차','연속','출루율','장타율','최근 10경기','년도','정규순위']]
     x = df7.팀
     y = df7.승률
     fig, ax = plt.subplots(figsize=(15,10))
