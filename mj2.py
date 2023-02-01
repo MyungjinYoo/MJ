a345cf56b605d247f6eb24d6c28a838fa43069a1
MyungjinYoo <123149344+MyungjinYoo@users.noreply.github.com>
2023-02-01 17:15:37 +0900

    Update mj2.py

--- mj2.py
+++ mj2.py
@@ -10,7 +10,7 @@ def  Monthly_economic_data():
     option = st.selectbox( 'How would you like to choice year ?', ('2020', '2021', '2022') )
     option2 = int(option)
     st.write('You selected:', option)
-    money = money[['Year','Month','$currency','US_interest','KOSPI','KOR_interest','brent_oil_price', 'House_price_index']]
+    money = money[['Year','Month','Dollar_currency','US_interest','KOSPI','Salary_Index','KOR_interest','brent_oil_price', 'House_price_index']]
     money = money[:] [money['Year']== option2]
     fig, ax = plt.subplots(2,2, figsize=(15,10))
     
