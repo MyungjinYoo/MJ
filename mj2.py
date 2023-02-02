d16b8d889987344856718aba76ae35d25267da10
MyungjinYoo <123149344+MyungjinYoo@users.noreply.github.com>
2023-02-02 16:02:34 +0900

    Update mj2.py

--- mj2.py
+++ mj2.py
@@ -66,7 +66,7 @@ def KBO_standings() :
     df7['게임차'] = round(df7.게임차,1)
     x = df7.팀
     y = df7.승률
-    fig, ax = plt.subplots(figsize=(15,10))
+    fig, ax = plt.subplots(figsize=(12,8))
     colors = ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7' ,'C8', 'C9', 'C10' ]
     plt.bar( x, y, color= colors , alpha = 0.6)
     for num , v in enumerate( y ):
